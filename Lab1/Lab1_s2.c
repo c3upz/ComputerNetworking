8c11b420928b90f9718f073607b19299e06f044d
c3upz <cgerbino@scu.edu>
2020-09-15 15:36:24 -0700

    trying

--- Lab1/Lab1_s2.c
+++ Lab1/Lab1_s2.c
@@ -5,36 +5,41 @@
     Description: This program exemplifies how 'forking' and processes work.
 
 */
+//git
 
-#include <stdio.h>      /* printf, stderr */
-#include <sys/types.h>  /* pid_t */
-#include <unistd.h>     /* fork */
-#include <stdlib.h>     /* atoi */
-#include <errno.h>      /* errno */ 
+#include <stdio.h>     /* printf, stderr */
+#include <sys/types.h> /* pid_t */
+#include <unistd.h>    /* fork */
+#include <stdlib.h>    /* atoi */
+#include <errno.h>     /* errno */
 /* main function with command-line arguments to pass */
-int main(int argc, char *argv[]) {
-    pid_t  pid;
+int main(int argc, char *argv[])
+{
+    pid_t pid;
     int i, n = atoi(argv[1]); // n microseconds to input from keyboard for delay
     printf("\n Before forking.\n");
     pid = fork();
-    if (pid == -1) {
+    if (pid == -1)
+    {
         fprintf(stderr, "can't fork, error %d\n", errno);
-    }    
-    if (pid){
+    }
+    if (pid)
+    {
         // Parent process
-        for (i=0;i<100;i++) {
-            printf("\t \t \t Parent Process %d \n",i);
+        for (i = 0; i < 100; i++)
+        {
+            printf("\t \t \t Parent Process %d \n", i);
             usleep(n);
         }
     }
-    else{
+    else
+    {
         // Child process
-        for (i=0;i<100;i++) {
-            printf("Child process %d\n",i);
+        for (i = 0; i < 100; i++)
+        {
+            printf("Child process %d\n", i);
             usleep(n);
         }
     }
     return 0;
 }
-
-
